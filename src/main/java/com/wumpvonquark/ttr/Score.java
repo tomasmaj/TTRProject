20a69ef4d304d3195e918f4335ef4f235800d6e4
Kza von Quark <linus.lundahl@gmail.com>
2016-06-21 10:31:59 +0200

    Refactored score and test

--- src/main/java/com/wumpvonquark/ttr/Score.java
+++ src/main/java/com/wumpvonquark/ttr/Score.java
@@ -5,40 +5,39 @@ package main.java.com.wumpvonquark.ttr;
  */
 public class Score {
 
-
-    public int trainRouteScore(Route route) {
-    int routeScore = 0;
-        switch (route.getLength()) {
-            case 1:
-                routeScore = 1;
-                break;
-            case 2:
-                routeScore = 2;
-                break;
-            case 3:
-                routeScore = 4;
-                break;
-            case 4:
-                routeScore = 7;
-                break;
-            case 6:
-                routeScore = 15;
-                break;
-            case 8:
-                routeScore = 21;
-                break;
-            default: routeScore = 0;
-        }
-        return routeScore;
-    }
-
     public int routesSum(int[] routes) {
         int routeSum = 0;
         for (int route : routes) {
-            routeSum += trainRouteScore(new Route(route));
+            routeSum += routeScore(new Route(route));
         }
         return routeSum;
     }
+
+        public int routeScore(Route route) {
+        int routeScore = 0;
+            switch (route.getLength()) {
+                case 1:
+                    routeScore = 1;
+                    break;
+                case 2:
+                    routeScore = 2;
+                    break;
+                case 3:
+                    routeScore = 4;
+                    break;
+                case 4:
+                    routeScore = 7;
+                    break;
+                case 6:
+                    routeScore = 15;
+                    break;
+                case 8:
+                    routeScore = 21;
+                    break;
+                default: routeScore = 0;
+            }
+            return routeScore;
+        }
 
     public int ticketSum(TicketCard[] tickets) {
         int ticketSum = 0;
--- src/test/java/com/wumpvonquark/ttr/ScoreTest.java
+++ src/test/java/com/wumpvonquark/ttr/ScoreTest.java
@@ -15,6 +15,8 @@ import static org.junit.Assert.assertTrue;
 public class ScoreTest {
 
     Score score;
+    int[] routes;
+    TicketCard[] tickets;
 
     @Before
     public void setUp() throws Exception {
@@ -24,39 +26,39 @@ public class ScoreTest {
     @Test
     public void scoreOneTileRoute() throws Exception {
         Route route = new Route(1);
-        assertEquals(1, score.trainRouteScore(route));
+        assertEquals(1, score.routeScore(route));
     }
 
     @Test
     public void scoreThreeTilesRoute() throws Exception {
         Route route = new Route(3);
-        assertEquals(4, score.trainRouteScore(route));
+        assertEquals(4, score.routeScore(route));
     }
 
     @Test
     public void scoreMultipleRoutes() throws Exception {
-        int[] routes = new int[] {6, 2, 3, 4, 1, 3};
+        routes = new int[] {6, 2, 3, 4, 1, 3};
         assertEquals(33, score.routesSum(routes));
     }
 
     @Test
     public void scoreBudapestToSofia() throws Exception {
-        int[] routes = {};
-        TicketCard[] tickets = {TicketCard.BUD_SOF};
+        routes = new int[]{};
+        tickets = new TicketCard[] {TicketCard.BUD_SOF};
         assertEquals(5, score.routesSum(routes) + score.ticketSum(tickets));
     }
 
     @Test
     public void scoreRouteAndTicketBudapestToSofia() throws Exception {
-        int[] routes = new int[] {3, 2};
-        TicketCard[] tickets = new TicketCard[] {TicketCard.BUD_SOF};
+        routes = new int[] {3, 2};
+        tickets = new TicketCard[] {TicketCard.BUD_SOF};
         assertEquals(11, score.routesSum(routes) + score.ticketSum(tickets));
     }
 
     @Test
     public void scoreMultipleTicketsAndRoutes() throws Exception {
-        TicketCard[] tickets = new TicketCard[] {TicketCard.BUD_SOF, TicketCard.LON_BER};
-        int[] routes = {3, 2, 2, 1, 3, 3};
+        routes = new int[] {3, 2, 2, 1, 3, 3};
+        tickets = new TicketCard[] {TicketCard.BUD_SOF, TicketCard.LON_BER};
         assertEquals(29, score.routesSum(routes) + score.ticketSum(tickets));
     }
 }
