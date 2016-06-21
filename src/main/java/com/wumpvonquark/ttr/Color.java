a94f830e9bdf188c3d4df574fb34668785ad3cf8
Kza von Quark <linus.lundahl@gmail.com>
2016-06-21 13:35:42 +0200

    More routes...

--- src/main/java/com/wumpvonquark/ttr/Color.java
+++ src/main/java/com/wumpvonquark/ttr/Color.java
@@ -5,5 +5,5 @@ package main.java.com.wumpvonquark.ttr;
  */
 public enum Color {
 
-    BLACK, WHITE, RED, GREEN, BLUE, YELLOW, ORANGE, PINK, OPTIONAL;
+    BLACK, WHITE, RED, GREEN, BLUE, YELLOW, ORANGE, PINK, OPTIONAL
 }
