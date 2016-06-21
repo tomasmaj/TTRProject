11a5e2a0d7495f538373917284f9a6cf51e5033f
tomasmaj <tmajling@gmail.com>
2016-06-21 11:04:29 +0200

    Added contructor and getter to City Enum

--- src/main/java/com/wumpvonquark/ttr/City.java
+++ src/main/java/com/wumpvonquark/ttr/City.java
@@ -1,5 +1,8 @@
 package main.java.com.wumpvonquark.ttr;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by Tomas Majling on 2016-06-20.
  */
@@ -7,6 +10,16 @@ public enum City {
 
     ATHINA, ANCORA, EDINGBURGH, ESSEN, KYIV, PARIS, WILNO, ZAGREB, BUDAPEST, SOFIA, BREST, VENEZIA,
     CONSTANTINOPLE, ZURICH, KHARKOV, MADRID, BERLIN, MOSKVA, ERZURUM, ROSTOV, LONDON, FRANKFURT,
-    KOBENHAVN, PARLERMO, DIEPPE, AMSTERDAM, BRINDISI, SMOLENSK, FRANKFRUT, PETROGRAD, MARSEILLE, RIGA, BUCURESTI, WIEN, STOCKHOLM;
+    KOBENHAVN, PARLERMO, DIEPPE, AMSTERDAM, BRINDISI, SMOLENSK, FRANKFRUT, PETROGRAD, MARSEILLE, RIGA, BUCURESTI, WIEN,
+    STOCKHOLM(KOBENHAVN, PETROGRAD);
 
+    private City[] cities;
+
+    City(City... cities) {
+        this.cities = cities;
+    }
+
+    public City[] getCities() {
+        return cities;
+    }
 }
