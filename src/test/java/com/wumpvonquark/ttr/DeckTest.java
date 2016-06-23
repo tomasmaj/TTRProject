9bb8bfb39c18af50009f9c9516ffed9e1cef4ed0
Kza von Quark <linus.lundahl@gmail.com>
2016-06-23 15:34:34 +0200

    Test for (not intended) route doubles

--- src/test/java/com/wumpvonquark/ttr/DeckTest.java
+++ src/test/java/com/wumpvonquark/ttr/DeckTest.java
@@ -1,17 +1,12 @@
 package test.java.com.wumpvonquark.ttr;
 
 import main.java.com.wumpvonquark.ttr.*;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import sun.security.krb5.internal.Ticket;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Tomas Majling on 2016-06-22.
@@ -53,5 +48,46 @@ public class DeckTest {
     public void deckOfRoutesShouldBeCorrectSize() throws Exception {
         deck = new RouteDeck();
         assertEquals(Route.values().length, deck.getDeck().size());
+    }
+
+    @Test
+    public void deckShouldNotContainDoubles() throws Exception {
+        deck = new RouteDeck();
+        assertTrue(containsDoubles(deck.getDeck()));
+    }
+
+    private boolean containsDoubles(Stack<Route> deck) {
+        int startPoint = 1;
+        List<City> cities = new ArrayList<>();
+        Color routeColor;
+        List<Route> doubleRoutes = new ArrayList<>();
+        doubleRoutes = getDoubles(doubleRoutes);
+        City city1;
+        City city2;
+        for (Route route : deck) {
+            cities.add(route.getCity1());
+            cities.add(route.getCity2());
+            routeColor = route.getColor();
+            for (int i = startPoint; i < deck.size(); i++) {
+                city1 = deck.elementAt(i).getCity1();
+                city2 = deck.elementAt(i).getCity2();
+                Color color = deck.elementAt(i).getColor();
+                if (cities.contains(city1) && cities.contains(city2) && routeColor == color && !doubleRoutes.contains(route) && !doubleRoutes.contains(route)) {
+                    System.out.println(deck.elementAt(i).toString());
+                    return false;
+                }
+            }
+            startPoint++;
+            cities.clear();
+        }
+        return true;
+    }
+
+    private List<Route> getDoubles(List<Route> doubleRoutes) {
+        doubleRoutes.add(Route.LON_DIE_OPT1);
+        doubleRoutes.add(Route.LON_DIE_OPT2);
+        doubleRoutes.add(Route.ESS_KOB_OPT1);
+        doubleRoutes.add(Route.ESS_KOB_OPT2);
+        return doubleRoutes;
     }
 }
